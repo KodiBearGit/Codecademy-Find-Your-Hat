37f546f00eb6f735f6d38b34f87b03bdea96dc4f
Chris <chris.tisserat@gmail.com>
2020-12-21 11:06:34 -0800

    working on moving logic

--- main.js
+++ main.js
@@ -6,5 +6,40 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-  
+  constructor(field) {
+    this.field = field;
+  }
+  print() {
+    console.log(this.field).toString());
+  }
 }
+let top = 0;
+let left = 0; 
+
+let move =  prompt('Which Way? u, d, l, or r');
+
+if (move === 'u') {
+  console.log('u');
+  if (top === 0) {
+    console.log("Out of Bounds");
+  }
+}
+else if (move === 'd') {
+
+} 
+else if (move === 'l') {
+
+}
+else if (move === 'r') {
+
+}
+else {
+  console.log("not a valid key");
+  move =  prompt('Make your move! u, d, l, or r');
+}
+//example of new Field class call.  
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);

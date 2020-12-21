3b75e0c2d65d134fd44112d9e3a6a4263b744281
Chris <chris.tisserat@gmail.com>
2020-12-21 13:13:31 -0800

    added comments to explain areas in code

--- main.js
+++ main.js
@@ -15,27 +15,30 @@ class Field {
     }
   }
 }
-let top = 0;
-let left = 0; 
 
+//function repsonds to a non-out of bounds move
 function checkMoveResult(top, left) {
+  //evaluates a move to hat "^" position
   if (myField.field[top][left] === '^') {
-    console.log(myField.field[top][left]);
     console.log("you found your hat!");
     return gameOver = true;
   } 
+  //evaluates a move to hole "O" position
   else if (myField.field[top][left] === 'O') {
-    console.log(myField.field[top][left]);
     console.log("you fell down the hole");
     return gameOver = true;
   }
+  //changes curent position to '+'
   else if (myField.field[top][left] === '░' || myField.field[top][left] === '*') {
-    myField.field[top][left] = '*';
-    console.log(myField.field[top][left]);
+    myField.field[top][left] = '+';
     return gameOver = false;
   }
 }
 
+//function changes previous position to '*'
+function moveASpace(top, left) {
+  myField.field[top][left] = '*';
+}
 //example of new Field class call.  
 const myField = new Field([
   ['*', '░', 'O'],
@@ -44,57 +47,70 @@ const myField = new Field([
   ['░', '░', '░'],
 ]);
 
-//console.log(myField.field.length);
-//console.log(myField.field[0].length);
+//sets starting postion of the game
+let top = 0;
+let left = 0; 
+
+//set initial position to  '+'
+myField.field[top][left] = '+';
 let gameOver = false;
 
+//loop  unknown number of iterations keeps running until gameOver = True.
 do {
   myField.print();
   let move =  prompt('Which Way? u, d, l, or r? ');
-
+  //user moves up "u"
   if (move === 'u') {
     if (top === 0) {
       console.log("Out of Bounds");
       gameOver = true;
     }
     else {
-        top -= 1;
-        checkMoveResult(top, left);
+      moveASpace(top, left)
+      top -= 1;
+      checkMoveResult(top, left);
     }
   }
+  //user moves down "d"
   else if (move === 'd') {
     if (top + 1 > myField.field.length) {
       console.log("Out of Bounds");
       gameOver = true;
     }
     else {
-        top += 1;
-        checkMoveResult(top, left);
+      moveASpace(top, left)
+      top += 1;
+      checkMoveResult(top, left);
     }
   } 
+  //user moves left "l"
   else if (move === 'l') {
     if (left === 0) {
       console.log("Out of Bounds");
       gameOver = true;
     }
     else {
-        left -= 1;
-        checkMoveResult(top, left);
+      moveASpace(top, left)
+      left -= 1;
+      checkMoveResult(top, left);
     }
   }
+  //user moves right "r"
   else if (move === 'r') {
     if (left === myField.field[0].length) {
       console.log("Out of Bounds, you lose");
       gameOver = true;
     }
     else {
-        left += 1;
-        checkMoveResult(top, left);
+      moveASpace(top, left)
+      left += 1;
+      checkMoveResult(top, left);
     }
   }
+  //user presses invalid key
   else {
     console.log("not a valid key");
-    move =  prompt('Make your move! u, d, l, or r');
+    gameOver === false
   }
 }  while (gameOver === false);
 
